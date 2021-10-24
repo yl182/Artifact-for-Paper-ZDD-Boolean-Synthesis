823554c2fa070b0fd2e135601c61ecf7846ef2c1
Yi Lin <yl182@rice.edu>
2021-10-24 03:47:18 -0500

    submission version

--- compilationRealizabilityWitnesses/dot2png.sh
+++
@@ -1,8 +0,0 @@
-for file in *.dot
-do
-	b="$(basename ${file} .dot)"
-	#echo $b
-	echo "Converting $b.dot to $b.png..."
-	dot $b.dot -Tpng -o $b.png
-	dot $b.dot -Tpdf -o $b.pdf
-done
--- compilationRealizabilityWitnesses/examplefiles/submit_robots.sh
+++
@@ -1,5 +0,0 @@
-for i in {1..10}
-do
-        export filename=cleaning_robots_${i}
-        sbatch run_sgrk.slurm
-done
